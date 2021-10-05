0ec40deaa413e9d21b924feadb2ec53396088fbf
jinu0124 <jinwoo6612@naver.com>
2021-10-06 04:04:12 +0900

    Fix: url 경로

--- backend/root/main.py
+++ backend/root/main.py
@@ -39,7 +39,7 @@ def init_app():
 app = init_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, loop="asyncio")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
     # gunicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
 # gunicorn main:app -k uvicorn.workers.UvicornWorker -p 8080:8080 -w 4
