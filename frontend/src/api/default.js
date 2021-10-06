42835449b8e2a8af75a99bc27b350bb2dc595fe3
1996skdms <1996skdms@gmail.com>
2021-10-06 22:26:46 +0900

    fix: baseUrl 변경

--- frontend/src/api/default.js
+++ frontend/src/api/default.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios'
 const _axios = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://j5a602.p.ssafy.io/api",
     timeout: 100000,
 })
 
